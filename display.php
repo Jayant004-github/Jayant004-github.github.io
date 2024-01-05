659f692f94e55aac6e8c23ee9deb3d2fd462b5bd
B Jayant <jayant.b2022@vitstudent.ac.in>
2024-01-05 16:46:56 +0530

    Update display.php

--- display.php
+++ display.php
@@ -7,8 +7,7 @@
 </head>
 <body>
     Congratulations!! You cracked the Flag.</br>
-    Decode the hidden message to reveal the flag. And insert the code.</br>
-    1N5P3C70R_0F_H7ML</br>
+    </br>
     passwd{insert}
 </body>
 </html>
