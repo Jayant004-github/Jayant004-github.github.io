00a7091048a6273e7ac247da09a43e5298f28de2
B Jayant <jayant.b2022@vitstudent.ac.in>
2024-01-05 17:06:51 +0530

    Update displayfailed1.php

--- displayfailed1.php
+++ displayfailed1.php
@@ -1,7 +1,54 @@
-<h1>Login failed.</h1>
-<pre>
+<html>
+<head>
+    <title>Login failed.</title>
+</head>
+<body>
+    <h1>Login failed.</h1>
+<script>
+        
+	// <![CDATA[  <-- For SVG support
+	if ('WebSocket' in window) {
+		(function () {
+			function refreshCSS() {
+				var sheets = [].slice.call(document.getElementsByTagName("link"));
+				var head = document.getElementsByTagName("head")[0];
+				for (var i = 0; i < sheets.length; ++i) {
+					var elem = sheets[i];
+					var parent = elem.parentElement || head;
+					parent.removeChild(elem);
+					var rel = elem.rel;
+					if (elem.href && typeof rel != "string" || rel.length == 0 || rel.toLowerCase() == "stylesheet") {
+						var url = elem.href.replace(/(&|\?)_cacheOverride=\d+/, '');
+						elem.href = url + (url.indexOf('?') >= 0 ? '&' : '?') + '_cacheOverride=' + (new Date().valueOf());
+					}
+					parent.appendChild(elem);
+				}
+			}
+			var protocol = window.location.protocol === 'http:' ? 'ws://' : 'wss://';
+			var address = protocol + window.location.host + window.location.pathname + '/ws';
+			var socket = new WebSocket(address);
+			socket.onmessage = function (msg) {
+				if (msg.data == 'reload') window.location.reload();
+				else if (msg.data == 'refreshcss') refreshCSS();
+			};
+			if (sessionStorage && !sessionStorage.getItem('IsThisFirstTime_Log_From_LiveServer')) {
+				console.log('Live reload enabled.');
+				sessionStorage.setItem('IsThisFirstTime_Log_From_LiveServer', true);
+			}
+		})();
+	}
+	else {
+		console.error('Upgrade your browser. This Browser is NOT supported WebSocket for Live-Reloading.');
+	}
+	// ]]>
+    </script>
+<script>
     username: 
     password: 
-    SQL query: SELECT * FROM users WHERE name=&#039;username&#039; AND password=&#039;password&#039;
+    SQL query: SELECT * FROM users WHERE username=&#039 &username &#039 && password=&#039 $password &#039;
     //' OR 1=1 --
-</pre>
+    
+</script>
+</body>
+</html>
+
